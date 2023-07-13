df368fbe19492d291773ac6950098a685b9c2bcc
agbld <agiblida@gmail.com>
2023-07-13 05:57:05 +0000

    eval

--- mf_from_scratch.py
+++ mf_from_scratch.py
@@ -1,12 +1,16 @@
 #%%
 # import packages
+
 import torch
 from torch import nn
 from torch.autograd import Variable
 import pandas as pd
+import numpy as np
+from tqdm import tqdm
 
 #%%
 # MF from scratch using PyTorch
+
 class MatrixFactorization(nn.Module):
     def __init__(self, n_users, n_items, n_factors=20):
         super().__init__()
@@ -21,14 +25,16 @@ class MatrixFactorization(nn.Module):
         return pred
 
 #%%
-# load data
-file_path = 'dataset/ml-1m/rating_tiny.dat'
+# Load data and preprocess
+
+# load ml-1m dataset
+file_path = 'dataset/ml-1m/ratings.dat' # rating_tiny.dat
 data = pd.read_csv(file_path, sep='::', names=['user_id', 'item_id', 'rating', 'timestamp'], engine='python')
 
 # remove duplicate data and keep the highest rating
 data = data.sort_values('rating', ascending=False).drop_duplicates(['user_id', 'item_id']).sort_index()
 
-# to consecutive user_id and item_id
+# to consecutive user_id and item_id (not necessary if using the complete dataset instead of the tiny one)
 user_ids = data['user_id'].unique()
 item_ids = data['item_id'].unique()
 user2idx = {o:i for i,o in enumerate(user_ids)}
@@ -51,35 +57,40 @@ test = data.loc[data['split'] == 'test', ['user_id', 'item_id', 'rating']]
 
 #%%
 # initialize model, loss and optimizer
-model = MatrixFactorization(n_users, n_items, n_factors=10)
+model = MatrixFactorization(n_users, n_items, n_factors=20)
 criterion = nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.05)
 
 #%%
 # train
 model.train()
-n_epochs = 10
+n_epochs = 50
 
-for epoch in range(n_epochs):
-    users = Variable(users)
-    items = Variable(items)
-    ratings = Variable(ratings)
-    
-    outputs = model(users, items)
-    loss = criterion(outputs, ratings)
-    
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-    
-    print ('Epoch [{}/{}], Loss: {:.4f}' .format(epoch+1, n_epochs, loss.item()))
+with tqdm(total=n_epochs) as pbar:
+    for epoch in range(n_epochs):
+        users = Variable(torch.Tensor(train['user_id'].values).long())
+        items = Variable(torch.Tensor(train['item_id'].values).long())
+        ratings = Variable(torch.Tensor(train['rating'].values))
+        
+        outputs = model(users, items)
+        loss = criterion(outputs, ratings)
+        
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        # print ('Epoch [{}/{}], Loss: {:.4f}' .format(epoch+1, n_epochs, loss.item()))
+        pbar.update(1)
+        pbar.set_postfix_str('Epoch [{}/{}], Loss: {:.4f}' .format(epoch+1, n_epochs, loss.item()))
 
 #%%
-# evaluate precision@k
+# evaluate model
 model.eval()
-with torch.no_grad():
-    outputs = model(users, items)
-    _, predicted = torch.topk(outputs, 3, dim=0)
-    print(predicted.t())
+users = Variable(torch.Tensor(test['user_id'].values).long())
+items = Variable(torch.Tensor(test['item_id'].values).long())
+ratings = Variable(torch.Tensor(test['rating'].values))
+outputs = model(users, items)
+loss = criterion(outputs, ratings)
+print('Test loss: {:.4f}'.format(loss.item()))
 
 #%%
